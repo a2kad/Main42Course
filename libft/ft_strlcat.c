ccf50b6045602d629fbbc9254f7dcea065ba8e1a
a2kad <reshetnikovruslan705@gmail.com>
2024-10-18 21:43:12 +0200

    strlcat done

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,10 +6,31 @@
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 13:16:21 by rureshet          #+#    #+#             */
-/*   Updated: 2024/10/18 13:18:05 by rureshet         ###   ########.fr       */
+/*   Updated: 2024/10/18 21:34:39 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 size_t	strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] && i < size)
+	{
+		i++;
+	}
+	while (src[j] && (i + j + 1) < size)
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i < size)
+	{
+		dst[i + j] = '\0';
+	}
+	return (i + ft_strlen(src));
+}
