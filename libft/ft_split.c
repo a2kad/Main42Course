9f6f57ec0cf2daa21a3e174c80ed8ce75f8cbbfd
a2kad <reshetnikovruslan705@gmail.com>
2024-10-27 16:29:42 +0100

    finished

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/25 21:24:34 by rureshet          #+#    #+#             */
-/*   Updated: 2024/10/25 21:44:57 by rureshet         ###   ########.fr       */
+/*   Created: 2024/10/19 17:19:54 by rureshet          #+#    #+#             */
+/*   Updated: 2024/10/27 14:57:01 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_initiate_vars(size_t *i, int *j, int *s_word)
+static void	ft_initiate_vars(size_t *i, int *j, int *start_word)
 {
 	*i = 0;
 	*j = 0;
-	*s_word = -1;
+	*start_word = -1;
 }
 
 static void	*ft_free(char **strs, int count)
@@ -78,22 +78,22 @@ char	**ft_split(const char *s, char c)
 	char	**res;
 	size_t	i;
 	int		j;
-	int		s_word;
+	int		start_word;
 
-	ft_initiate_vars(&i, &j, &s_word);
+	ft_initiate_vars(&i, &j, &start_word);
 	res = ft_calloc((word_count(s, c) + 1), sizeof(char *));
 	if (!res)
 		return (NULL);
 	while (i <= ft_strlen(s))
 	{
-		if (s[i] != c && s_word < 0)
-			s_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && s_word >= 0)
+		if (s[i] != c && start_word < 0)
+			start_word = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && start_word >= 0)
 		{
-			res[j] = fill_word(s, s_word, i);
+			res[j] = fill_word(s, start_word, i);
 			if (!(res[j]))
 				return (ft_free(res, j));
-			s_word = -1;
+			start_word = -1;
 			j++;
 		}
 		i++;
