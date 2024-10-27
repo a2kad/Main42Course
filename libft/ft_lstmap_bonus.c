1ecdcb778a789ffb56ec484b7afe0c876224b668
a2kad <reshetnikovruslan705@gmail.com>
2024-10-27 14:33:50 +0100

    finished

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 14:10:44 by rureshet          #+#    #+#             */
-/*   Updated: 2024/10/26 23:03:17 by rureshet         ###   ########.fr       */
+/*   Updated: 2024/10/27 13:39:53 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_obj;
+	void	*new_cont;
 
 	new_list = NULL;
 	if (lst == NULL || f == NULL)
 		return (NULL);
 	while (lst)
 	{
-		new_obj = ft_lstnew(f(lst->content));
-		if (new_obj == NULL)
+		new_cont = f(lst->content);
+		new_obj = ft_lstnew(new_cont);
+		if (!new_obj)
 		{
+			del(new_cont);
 			ft_lstclear(&new_list, del);
+			lst = NULL;
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_obj);
