baa1f326e334bb97bb1577e7bc6833545ce2336a
a2kad <reshetnikovruslan705@gmail.com>
2024-11-03 18:54:14 +0100

    headers added

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,13 @@
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 15:39:53 by rureshet          #+#    #+#             */
-/*   Updated: 2024/11/03 15:39:54 by rureshet         ###   ########.fr       */
+/*   Updated: 2024/11/03 15:46:47 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,15 @@
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 15:39:59 by rureshet          #+#    #+#             */
-/*   Updated: 2024/11/03 15:40:00 by rureshet         ###   ########.fr       */
+/*   Updated: 2024/11/03 15:55:39 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
