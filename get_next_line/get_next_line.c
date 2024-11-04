abf69d331f1d4d34513f37b40ee54ced888c6a0d
a2kad <reshetnikovruslan705@gmail.com>
2024-11-04 15:03:00 +0100

    start

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,32 @@
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 15:39:53 by rureshet          #+#    #+#             */
-/*   Updated: 2024/11/03 15:46:47 by rureshet         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:51:06 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
+	size_t	rf;
+	char	*txt;
+	int i;
 
+	i = 0;
+	txt = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	rf = read(fd, txt, BUFFER_SIZE);
+	printf("%s", txt);
+	printf("\n%zu", rf);
+	return (txt);
+}
+
+int	main(void)
+{
+	int	fd;
+	char	*txt;
+	fd = open("test.txt", O_RDONLY);
+	txt = get_next_line(fd);
+	printf("%s", txt);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,16 @@
 /*   By: rureshet <rureshet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 15:39:59 by rureshet          #+#    #+#             */
-/*   Updated: 2024/11/03 15:55:39 by rureshet         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:48:41 by rureshet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# define BUFFER_SIZE 50
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
 
 char	*get_next_line(int fd);
 
